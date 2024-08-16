8335491667a0a0253309156093546bdcf8382dbc
Niyar Rabe <108587173+Niyar07@users.noreply.github.com>
2024-08-16 13:03:47 +0530

    Add files via upload

---
+++ 3.0/Codes C++/sherlock_Anagram.cpp
@@ -0,0 +1,93 @@
+//  hackerrank Questions
+
+#include <bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'sherlockAndAnagrams' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int sherlockAndAnagrams(string s)
+{
+    unordered_map<string, int> anagramic_substing_count;
+    int anagram_pair = 0;
+
+    // Generate all Substrings
+    for (int i = 0; i < s.length(); i++)
+    {
+        for (int j = i; j < s.length(); j++)
+        {
+            string substring = s.substr(i, j - i + 1);
+            sort(substring.begin(), substring.end());
+
+            // update count in map
+            anagramic_substing_count[substring]++;
+        }
+    }
+
+    // calculate pair
+
+    for (auto i : anagramic_substing_count)
+    {
+        int count = i.second;
+        if (count > 1)
+        {
+            // find pairs
+            anagram_pair += (count * (count - 1)) / 2;
+        }
+    }
+    return anagram_pair;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++)
+    {
+        string s;
+        getline(cin, s);
+
+        int result = sherlockAndAnagrams(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+
+    return s;
+}
+
+string rtrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end());
+
+    return s;
+}
